62f6400898a169ee44b6d55d85a77075da02aebf
Demishev <konstantind@2kgroup.com>
2014-04-15 12:16:31 +0300

    Game stuff from previous HoldemDojo moved here.

---
+++ src/main/java/com/robopoker/gameStuff/Card.java
@@ -0,0 +1,70 @@
+package com.robopoker.gameStuff;
+
+/**
+ * User: Demishev
+ * Date: 15.04.2014
+ * Time: 11:24
+ */
+public class Card implements Comparable<Card> {
+    private final CardSuit cardSuit;
+    private final CardValue cardValue;
+
+    public Card(CardSuit cardSuit, CardValue cardValue) {
+        this.cardSuit = cardSuit;
+        this.cardValue = cardValue;
+    }
+
+    CardSuit getCardSuit() {
+        return cardSuit;
+    }
+
+    public CardValue getCardValue() {
+        return cardValue;
+    }
+
+    public String getCardValueName() {
+        return cardValue.getFullName();
+    }
+
+    @Override
+    public String toString() {
+        return "" + cardValue + cardSuit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        return cardSuit == card.cardSuit && cardValue == card.cardValue;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cardSuit != null ? cardSuit.hashCode() : 0;
+        result = 31 * result + (cardValue != null ? cardValue.hashCode() : 0);
+        return result;
+    }
+
+
+    public int compareTo(Card o) {
+        return cardValue.compareTo(o.getCardValue());
+    }
+
+    public boolean sameSuit(Card otherCard) {
+        return cardSuit == otherCard.getCardSuit();
+    }
+
+    public boolean isNear(Card otherCard) {
+        int cardValueNumber = cardValue.ordinal();
+        int otherCardValue = otherCard.cardValue.ordinal();
+        return Math.abs(cardValueNumber - otherCardValue) == 1;
+    }
+
+    public boolean sameValue(Card otherCard) {
+        return cardValue == otherCard.getCardValue();
+    }
+}
